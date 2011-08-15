4e6d3a9be61ae58f7efbd2e71abdc359abaf9a7c
Oleg M <twik@github.com>
2011-08-15 13:27:41 -0700

    4 spaces indentation for *.html files

--- init.el
+++ init.el
@@ -318,3 +318,8 @@
               (kill-region (point) (mark)))
       (insert (mac-paste-function)))
 
+;; ---
+(add-hook 'html-mode-hook
+  (lambda ()
+    ;; Default indentation is usually 2 spaces, changing to 4.
+    (set (make-local-variable 'sgml-basic-offset) 4)))
