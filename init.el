c34cf3c93cbed094d4503759d262823ae4b5b218
Oleg M <twik@github.com>
2011-12-13 17:09:25 -0800

    removed textmate mode

--- init.el
+++ init.el
@@ -113,10 +113,6 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-(require 'textmate)
-(textmate-mode)
-
 ;; --------------
 (defvar LIMIT 1)
 (defvar time 0)
